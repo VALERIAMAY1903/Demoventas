2e22aa450a2ebe51dbb2138a944985b026cfd2d4
VALERIAMAY1903 <flugo2710@gmail.com>
2025-03-10 21:09:42 -0600

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+# Asegúrate de que el archivo 'SalidaFinalVentas.xlsx' esté en el mismo directorio
+# que tu script de Streamlit o especifica la ruta completa.
+
+try:
+  df = pd.read_excel('SalidaFinalVentas.xlsx')
+  st.dataframe(df)  # Muestra el DataFrame en la aplicación de Streamlit
+except FileNotFoundError:
+  st.error("Error: El archivo 'SalidaFinalVentas.xlsx' no se encontró.")
+except Exception as e:
+  st.error(f"Ocurrió un error al leer el archivo: {e}")
